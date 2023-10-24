dcffff53a6635192b93b533e50deeded73b10a2d
mhiggins8450 <146852125+mhiggins8450@users.noreply.github.com>
2023-10-24 19:56:40 -0400

    initial commit and completion

---
+++ w2d4labhw.js
@@ -0,0 +1,39 @@
+// Write a while loop for the given output:
+//  10, 30, 50, 70, 90
+//  20, 40, 60, 80, 100
+
+// This prints out the numbers in sequential order
+// let x = 10; {
+//     while (x <= 100) {
+//       console.log(x);
+//       x += 10;
+//     }
+//   }
+
+// This prints out the odds first and then the evens
+// let i = 10;
+
+// while (i <= 100) {
+//     console.log(i);
+//     i += 20;
+// }
+
+// let j = 20;
+
+// while (j <= 100) {
+//     console.log(j);
+//     j += 20;
+// }
+
+// Write a while loop that prints all 
+//numbers from 500 - 800 only if they 
+//are divisible by 4.
+
+// let i = 500;
+
+// while (i <= 800) {
+//     if (i % 4 === 0) {
+//         console.log(i);
+//     }
+//     i++;
+// }
